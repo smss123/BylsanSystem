e40120f526bf42f7ce61e9f71e84377dd778758a
Abu Ehab <MyGlassBox@Hotmail.Com>
2014-11-18 15:08:00 +0200

    Accontant Part1

---
+++ Solution1/XamaDataLayer/Accountant/AccountCategoryCmd.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamaDataLayer.Accountant
+{
+   public static  class AccountCategoryCmd
+    {
+       static DbDataContext db = new DbDataContext();
+       public static bool AddAccountCategory(AccountCategory tb)
+       {
+           db = new DbDataContext();
+           db.AccountCategories.InsertOnSubmit(tb);
+           db.SubmitChanges();
+           return true;
+       }
+
+
+       public static AccountCategory EditAccountCategory(AccountCategory tb, int xid)
+       {
+           db = new DbDataContext();
+           var act = db.AccountCategories.Where(a => a.ID == xid).SingleOrDefault();
+           act.AccountCategoryName = tb.AccountCategoryName;
+           act.Description = tb.Description;
+
+           db.SubmitChanges();
+           return act;
+       }
+
+
+       public static void DeleteAccountCategory(int xid)
+       {
+           try
+           {
+           db = new DbDataContext();
+           var act = db.AccountCategories.Where(a => a.ID == xid).SingleOrDefault();
+           db.AccountCategories.DeleteOnSubmit(act);
+
+           db.SubmitChanges();
+           }
+           catch (Exception)
+           {
+               
+               throw;
+           }
+       }
+
+       public static List<AccountCategory> GetAll()
+       {
+           db = new DbDataContext();
+           return db.AccountCategories.ToList();
+       }
+    }
+}
---
+++ Solution1/XamaDataLayer/Accountant/AccountsCmd.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamaDataLayer.Accountant
+{
+    public static class AccountsCmd
+    {
+        static DbDataContext db = new DbDataContext();
+        public static bool AddAccount(Account tb)        
+        {
+            // عارف أنه معمول له تريـــقر  ومش راح يلزم
+            db = new DbDataContext();
+            db.Accounts.InsertOnSubmit(tb);
+            db.SubmitChanges();
+            return true;
+        }
+
+        public static Account EditAccount(Account tb, int xid)
+        {
+            db = new DbDataContext();
+            var act = db.Accounts.Where(a => a.ID == xid).SingleOrDefault();
+            act.CategoryID = tb.CategoryID;
+            act.AccountName = tb.AccountName;
+            act.Description = tb.Description;
+
+            db.SubmitChanges();
+            return act;
+        }
+
+        public static void DeleteAccount(int xid)
+        {
+            try
+            {
+            db = new DbDataContext();
+            var act = db.Accounts.Where(a => a.ID == xid).SingleOrDefault();
+            db.Accounts.DeleteOnSubmit(act);
+
+            db.SubmitChanges();
+            
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        public static List<Account> GetAllAccounts()
+        {
+            db = new DbDataContext();
+            return db.Accounts.ToList();
+        }
+
+        public static List<Account> GetAccountByCategoryID(int categID)
+        {
+            db = new DbDataContext();
+            var ACT = (from g in db.Accounts where g.CategoryID == categID select g).ToList();
+            return ACT;
+        }
+
+
+    }
+}
