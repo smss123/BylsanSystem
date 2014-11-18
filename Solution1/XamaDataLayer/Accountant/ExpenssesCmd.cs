7ec498d4b6f17afd253efdf29991d1e049642fe3
Abu Ehab <MyGlassBox@Hotmail.Com>
2014-11-18 15:57:44 +0200

    Accountant Part2

---
+++ Solution1/XamaDataLayer/Accountant/ExpenssesCmd.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamaDataLayer.Accountant
+{
+    public static     class ExpenssesCmd
+    {
+        static DbDataContext db = new DbDataContext();
+        public static bool AddExpensses(Expenss tb)
+        {
+            db = new DbDataContext();
+            db.Expensses.InsertOnSubmit(tb);
+            db.SubmitChanges();
+            return true;
+        }
+
+
+        public static Expenss EditExpenss(Expenss tb, int xid)
+        {
+            db = new DbDataContext();
+            var exp = db.Expensses.Where(x => x.ID == xid).SingleOrDefault();
+            exp.ExpenssesName = tb.ExpenssesName;
+            exp.Description = tb.Description;
+            exp.AccountID = tb.AccountID;
+
+            db.SubmitChanges();
+            return exp;
+        }
+
+
+        public static void DeleteExpenses(int xid)
+        {
+            try
+            {
+            db = new DbDataContext();
+            var exp = db.Expensses.Where(x => x.ID == xid).SingleOrDefault();
+            db.Expensses.DeleteOnSubmit(exp);
+            db.SubmitChanges();
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        public static List<Expenss> GetAllExpensses()
+        {
+            db = new DbDataContext();
+            return db.Expensses.ToList();
+        }
+
+    }
+}
---
+++ Solution1/XamaDataLayer/Accountant/ExpenssesMovmentCmd.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamaDataLayer.Accountant
+{
+    public static class ExpenssesMovmentCmd
+    {
+        static DbDataContext db = new DbDataContext();
+        public static bool AddExpenssesMovment( ExpenssesMovment tb )
+        {
+            db = new DbDataContext();
+            db.ExpenssesMovments.InsertOnSubmit(tb);
+            db.SubmitChanges();
+            return true;
+        }
+        public static ExpenssesMovment EditExpMovment(ExpenssesMovment tb, int xid)
+        {
+            db = new DbDataContext();
+            var exp = db .ExpenssesMovments .Where (x=> x.ID == xid ).SingleOrDefault ();
+            exp .ExpenssesID = tb .ExpenssesID ;
+            exp .Amount = tb .Amount ;
+            exp .DateOfProcess = tb .DateOfProcess ;
+            exp .Description = tb .Description  ;
+
+
+
+            db .SubmitChanges ();
+            return exp;
+        }
+
+        public static void DeleteExpMovment(int xid)
+        {
+            try
+            {
+            db = new DbDataContext();
+            var exp = db .ExpenssesMovments .Where (x=> x.ID == xid ).SingleOrDefault ();
+            db.ExpenssesMovments.DeleteOnSubmit(exp);
+            db.SubmitChanges();
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        public static List<ExpenssesMovment> GetAll()
+        {
+            db = new DbDataContext();
+            return db.ExpenssesMovments.ToList();
+        }
+
+
+        public static List<ExpenssesMovment> GetAllExpnssesMovmentsByDate( DateTime dat )
+        {
+            db = new DbDataContext();
+            var lst = (from d in db.ExpenssesMovments where d.DateOfProcess == dat select d).ToList();
+            return lst;
+           
+        }
+
+
+
+
+    }
+}
