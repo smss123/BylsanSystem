925c1c6bab41e82153b715164114a9d18208e84f
smss123 <smsssamer@live.com>
2014-11-19 09:48:35 +0400

    loader

---
+++ Solution1/Bylsan System/EmployeeForms/FrmAddEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bylsan_System.EmployeeForms
+{
+    public partial class FrmAddEmployee : Form
+    {
+        public FrmAddEmployee()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
