f580eecd42bb00a50d6b8041627dcf26c55fdb80
ainma007 <b-sword@outlook.com>
2014-11-20 13:13:37 +0200

    new

--- Solution1/Bylsan System/CustomerForms/FrmCustomerManage.Designer.cs
+++ Solution1/Bylsan System/CustomerForms/FrmCustomerManage.Designer.cs
@@ -28,22 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCustomerManage));
+            Telerik.WinControls.UI.GridViewDecimalColumn gridViewDecimalColumn1 = new Telerik.WinControls.UI.GridViewDecimalColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn1 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn2 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewDateTimeColumn gridViewDateTimeColumn1 = new Telerik.WinControls.UI.GridViewDateTimeColumn();
+            Telerik.WinControls.UI.GridViewDecimalColumn gridViewDecimalColumn2 = new Telerik.WinControls.UI.GridViewDecimalColumn();
+            Telerik.WinControls.UI.GridViewCommandColumn gridViewCommandColumn1 = new Telerik.WinControls.UI.GridViewCommandColumn();
+            Telerik.WinControls.UI.GridViewCommandColumn gridViewCommandColumn2 = new Telerik.WinControls.UI.GridViewCommandColumn();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radGridView1 = new Telerik.WinControls.UI.RadGridView();
+            this.CoustomerGridView = new Telerik.WinControls.UI.RadGridView();
+            this.customerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.windows8Theme1 = new Telerik.WinControls.Themes.Windows8Theme();
+            this.toolStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CoustomerGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CoustomerGridView.MasterTemplate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // toolStrip1
             // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(848, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(104, 22);
+            this.toolStripButton1.Text = "Add Customer";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(93, 22);
+            this.toolStripButton2.Text = "Refresh Data";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(62, 22);
+            this.toolStripButton3.Text = "Report";
             // 
             // statusStrip1
             // 
@@ -55,23 +99,94 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.radGridView1);
+            this.groupBox1.Controls.Add(this.CoustomerGridView);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox1.Location = new System.Drawing.Point(0, 25);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(848, 476);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "CustomerData";
             // 
-            // radGridView1
+            // CoustomerGridView
             // 
-            this.radGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.radGridView1.Location = new System.Drawing.Point(3, 16);
-            this.radGridView1.Name = "radGridView1";
-            this.radGridView1.Size = new System.Drawing.Size(842, 457);
-            this.radGridView1.TabIndex = 0;
-            this.radGridView1.Text = "radGridView1";
+            this.CoustomerGridView.BackColor = System.Drawing.SystemColors.Control;
+            this.CoustomerGridView.Cursor = System.Windows.Forms.Cursors.Default;
+            this.CoustomerGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CoustomerGridView.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.CoustomerGridView.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.CoustomerGridView.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.CoustomerGridView.Location = new System.Drawing.Point(3, 16);
+            // 
+            // CoustomerGridView
+            // 
+            this.CoustomerGridView.MasterTemplate.AllowAddNewRow = false;
+            this.CoustomerGridView.MasterTemplate.AllowColumnReorder = false;
+            this.CoustomerGridView.MasterTemplate.AutoGenerateColumns = false;
+            this.CoustomerGridView.MasterTemplate.AutoSizeColumnsMode = Telerik.WinControls.UI.GridViewAutoSizeColumnsMode.Fill;
+            gridViewDecimalColumn1.DataType = typeof(int);
+            gridViewDecimalColumn1.EnableExpressionEditor = false;
+            gridViewDecimalColumn1.FieldName = "ID";
+            gridViewDecimalColumn1.HeaderText = "ID";
+            gridViewDecimalColumn1.IsAutoGenerated = true;
+            gridViewDecimalColumn1.Name = "ID";
+            gridViewDecimalColumn1.Width = 99;
+            gridViewTextBoxColumn1.EnableExpressionEditor = false;
+            gridViewTextBoxColumn1.FieldName = "CustomerName";
+            gridViewTextBoxColumn1.HeaderText = "CustomerName";
+            gridViewTextBoxColumn1.IsAutoGenerated = true;
+            gridViewTextBoxColumn1.Name = "CustomerName";
+            gridViewTextBoxColumn1.Width = 230;
+            gridViewTextBoxColumn2.EnableExpressionEditor = false;
+            gridViewTextBoxColumn2.FieldName = "PhoneNumber";
+            gridViewTextBoxColumn2.HeaderText = "PhoneNumber";
+            gridViewTextBoxColumn2.IsAutoGenerated = true;
+            gridViewTextBoxColumn2.Name = "PhoneNumber";
+            gridViewTextBoxColumn2.Width = 138;
+            gridViewDateTimeColumn1.DataType = typeof(System.Nullable<System.DateTime>);
+            gridViewDateTimeColumn1.EnableExpressionEditor = false;
+            gridViewDateTimeColumn1.FieldName = "CreateDate";
+            gridViewDateTimeColumn1.Format = System.Windows.Forms.DateTimePickerFormat.Long;
+            gridViewDateTimeColumn1.HeaderText = "CreateDate";
+            gridViewDateTimeColumn1.IsAutoGenerated = true;
+            gridViewDateTimeColumn1.Name = "CreateDate";
+            gridViewDateTimeColumn1.Width = 132;
+            gridViewDecimalColumn2.DataType = typeof(System.Nullable<int>);
+            gridViewDecimalColumn2.EnableExpressionEditor = false;
+            gridViewDecimalColumn2.FieldName = "Points";
+            gridViewDecimalColumn2.HeaderText = "Points";
+            gridViewDecimalColumn2.IsAutoGenerated = true;
+            gridViewDecimalColumn2.Name = "Points";
+            gridViewDecimalColumn2.Width = 105;
+            gridViewCommandColumn1.EnableExpressionEditor = false;
+            gridViewCommandColumn1.HeaderText = "Edit";
+            gridViewCommandColumn1.Name = "EditCol";
+            gridViewCommandColumn1.Width = 59;
+            gridViewCommandColumn2.EnableExpressionEditor = false;
+            gridViewCommandColumn2.HeaderText = "Delete";
+            gridViewCommandColumn2.Name = "DeleteCol";
+            gridViewCommandColumn2.Width = 64;
+            this.CoustomerGridView.MasterTemplate.Columns.AddRange(new Telerik.WinControls.UI.GridViewDataColumn[] {
+            gridViewDecimalColumn1,
+            gridViewTextBoxColumn1,
+            gridViewTextBoxColumn2,
+            gridViewDateTimeColumn1,
+            gridViewDecimalColumn2,
+            gridViewCommandColumn1,
+            gridViewCommandColumn2});
+            this.CoustomerGridView.MasterTemplate.DataSource = this.customerBindingSource;
+            this.CoustomerGridView.MasterTemplate.EnableFiltering = true;
+            this.CoustomerGridView.Name = "CoustomerGridView";
+            this.CoustomerGridView.ReadOnly = true;
+            this.CoustomerGridView.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.CoustomerGridView.Size = new System.Drawing.Size(842, 457);
+            this.CoustomerGridView.TabIndex = 0;
+            this.CoustomerGridView.Text = "radGridView1";
+            this.CoustomerGridView.ThemeName = "Windows8";
+            // 
+            // customerBindingSource
+            // 
+            this.customerBindingSource.DataSource = typeof(XamaDataLayer.Customer);
             // 
             // FrmCustomerManage
             // 
@@ -83,9 +198,12 @@
             this.Controls.Add(this.toolStrip1);
             this.Name = "FrmCustomerManage";
             this.Text = "FrmCustomerManage";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CoustomerGridView.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CoustomerGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,6 +214,11 @@
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private Telerik.WinControls.UI.RadGridView radGridView1;
+        private Telerik.WinControls.UI.RadGridView CoustomerGridView;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.BindingSource customerBindingSource;
+        private Telerik.WinControls.Themes.Windows8Theme windows8Theme1;
     }
 }
