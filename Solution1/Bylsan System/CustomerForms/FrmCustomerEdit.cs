02a30a87ed6b249ca3bd0fda5fef77300aa8507f
Abu Ehab <MyGlassBox@Hotmail.Com>
2014-11-22 17:48:11 +0200

    Populate Edit Customer's Form By Code

--- Solution1/Bylsan System/CustomerForms/FrmCustomerEdit.cs
+++ Solution1/Bylsan System/CustomerForms/FrmCustomerEdit.cs
@@ -20,5 +20,46 @@ namespace Bylsan_System.CustomerForms
         {
             InitializeComponent();
         }
+
+
+        public int CustmrID { get; set; }
+        private void Addbtn_Click(object sender, EventArgs e)
+        {
+            //#region "  CheckFillTextBox "
+
+            //if (customerNameTextBox.Text == "")
+            //{
+
+            //    customerNameTextBox.BackColor = Color.OrangeRed;
+
+            //    customerNameTextBox.Focus();
+            //    errorProvider1.SetError(this.customerNameTextBox, "Please Enter Branch name");
+
+            //    return;
+            //}
+            //else
+            //{
+            //    customerNameTextBox.BackColor = Color.White;
+            //    errorProvider1.Clear();
+
+            //}
+
+            //#endregion
+
+          CustmrID = 0;
+          CustmrID = CustomersCmd.GetCustomerIDByName(customerNameTextBox.Text);
+          if(CustmrID != 0){
+
+            Customer tb = new Customer();
+            tb.CustomerName = customerNameTextBox.Text;
+            tb.PhoneNumber = phoneNumberTextBox.Text;
+            CustomersCmd.EditCustomer (tb,CustmrID );
+            MessageBox.Show("Updated ...");
+            this.Hide();
+
+
+          }
+
+        }
     }
 }
