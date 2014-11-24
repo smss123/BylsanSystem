24c7128468c6a3a2eaef6845079f0d9661a25a0d
Abu Ehab <MyGlassBox@Hotmail.Com>
2014-11-25 00:26:04 +0200

    Order 1.3

--- Solution1/Bylsan System/SenarioAddOrderForms/FrmTotalOrder.Designer.cs
+++ Solution1/Bylsan System/SenarioAddOrderForms/FrmTotalOrder.Designer.cs
@@ -29,17 +29,25 @@
         private void InitializeComponent()
         {
             this.CustomerInfomationGroupBox = new System.Windows.Forms.GroupBox();
-            this.TotalOrderListView = new Telerik.WinControls.UI.RadListView();
+            this.NameLab = new System.Windows.Forms.Label();
+            this.PhoneLab = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.radButton1 = new Telerik.WinControls.UI.RadButton();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.TotalOrderListView)).BeginInit();
+            this.OkeyBtn = new Telerik.WinControls.UI.RadButton();
+            this.ListViewProductes = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CustomerInfomationGroupBox.SuspendLayout();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OkeyBtn)).BeginInit();
             this.SuspendLayout();
             // 
             // CustomerInfomationGroupBox
             // 
+            this.CustomerInfomationGroupBox.Controls.Add(this.label3);
+            this.CustomerInfomationGroupBox.Controls.Add(this.label2);
+            this.CustomerInfomationGroupBox.Controls.Add(this.NameLab);
+            this.CustomerInfomationGroupBox.Controls.Add(this.PhoneLab);
             this.CustomerInfomationGroupBox.Dock = System.Windows.Forms.DockStyle.Top;
             this.CustomerInfomationGroupBox.Location = new System.Drawing.Point(0, 0);
             this.CustomerInfomationGroupBox.Name = "CustomerInfomationGroupBox";
@@ -48,32 +56,33 @@
             this.CustomerInfomationGroupBox.TabStop = false;
             this.CustomerInfomationGroupBox.Text = "Customer Infomation";
             // 
-            // TotalOrderListView
+            // NameLab
             // 
-            this.TotalOrderListView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.TotalOrderListView.Location = new System.Drawing.Point(0, 86);
-            this.TotalOrderListView.Name = "TotalOrderListView";
-            this.TotalOrderListView.Size = new System.Drawing.Size(479, 352);
-            this.TotalOrderListView.TabIndex = 8;
-            this.TotalOrderListView.Text = "radListView1";
+            this.NameLab.AutoSize = true;
+            this.NameLab.Location = new System.Drawing.Point(127, 31);
+            this.NameLab.Name = "NameLab";
+            this.NameLab.Size = new System.Drawing.Size(35, 13);
+            this.NameLab.TabIndex = 1;
+            this.NameLab.Text = "label3";
+            // 
+            // PhoneLab
+            // 
+            this.PhoneLab.AutoSize = true;
+            this.PhoneLab.Location = new System.Drawing.Point(127, 53);
+            this.PhoneLab.Name = "PhoneLab";
+            this.PhoneLab.Size = new System.Drawing.Size(35, 13);
+            this.PhoneLab.TabIndex = 0;
+            this.PhoneLab.Text = "label2";
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.radButton1);
+            this.panel1.Controls.Add(this.OkeyBtn);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel1.Location = new System.Drawing.Point(0, 394);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(479, 44);
             this.panel1.TabIndex = 9;
-            // 
-            // radButton1
-            // 
-            this.radButton1.Location = new System.Drawing.Point(338, 6);
-            this.radButton1.Name = "radButton1";
-            this.radButton1.Size = new System.Drawing.Size(132, 33);
-            this.radButton1.TabIndex = 0;
-            this.radButton1.Text = "radButton1";
             // 
             // label1
             // 
@@ -84,20 +93,60 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
+            // OkeyBtn
+            // 
+            this.OkeyBtn.Location = new System.Drawing.Point(338, 6);
+            this.OkeyBtn.Name = "OkeyBtn";
+            this.OkeyBtn.Size = new System.Drawing.Size(132, 33);
+            this.OkeyBtn.TabIndex = 0;
+            this.OkeyBtn.Text = "Ok";
+            // 
+            // ListViewProductes
+            // 
+            this.ListViewProductes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ListViewProductes.FullRowSelect = true;
+            this.ListViewProductes.GridLines = true;
+            this.ListViewProductes.Location = new System.Drawing.Point(0, 86);
+            this.ListViewProductes.Name = "ListViewProductes";
+            this.ListViewProductes.Size = new System.Drawing.Size(479, 308);
+            this.ListViewProductes.TabIndex = 10;
+            this.ListViewProductes.UseCompatibleStateImageBehavior = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Customer Name ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Customer Phone";
+            // 
             // FrmTotalOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(479, 438);
+            this.Controls.Add(this.ListViewProductes);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.TotalOrderListView);
             this.Controls.Add(this.CustomerInfomationGroupBox);
             this.Name = "FrmTotalOrder";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmTotalOrder";
-            ((System.ComponentModel.ISupportInitialize)(this.TotalOrderListView)).EndInit();
+            this.Load += new System.EventHandler(this.FrmTotalOrder_Load_1);
+            this.CustomerInfomationGroupBox.ResumeLayout(false);
+            this.CustomerInfomationGroupBox.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OkeyBtn)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -105,9 +154,13 @@
         #endregion
 
         private System.Windows.Forms.GroupBox CustomerInfomationGroupBox;
-        private Telerik.WinControls.UI.RadListView TotalOrderListView;
         private System.Windows.Forms.Panel panel1;
-        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadButton OkeyBtn;
         private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.ListView ListViewProductes;
+        public System.Windows.Forms.Label NameLab;
+        public System.Windows.Forms.Label PhoneLab;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Solution1/Bylsan System/SenarioAddOrderForms/FrmTotalOrder.cs
+++ Solution1/Bylsan System/SenarioAddOrderForms/FrmTotalOrder.cs
@@ -8,13 +8,43 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using XamaDataLayer.BranchCmd;
+using XamaDataLayer.Helper_Classes;
+
 namespace Bylsan_System.SenarioAddOrderForms
-{
+{ // 1
     public partial class FrmTotalOrder : Form
-    {
+    {//2
         public FrmTotalOrder()
         {
             InitializeComponent();
         }
+
+        #region "Populate ListProducts "
+
+        void CreateListViewProducts()
+        {
+
+            ListViewProductes.View = View.Details;
+
+            ListViewProductes.Columns.Add("ID", 30, HorizontalAlignment.Center);
+
+            ListViewProductes.Columns.Add("Product", 230, HorizontalAlignment.Center);
+
+            ListViewProductes.Columns.Add("Descriptoin", 300, HorizontalAlignment.Center);
+
+
+        }
+
+        #endregion
+
+
+        private void FrmTotalOrder_Load_1(object sender, EventArgs e)
+        {
+            CreateListViewProducts();
+        }
+
+
+
     }
 }
