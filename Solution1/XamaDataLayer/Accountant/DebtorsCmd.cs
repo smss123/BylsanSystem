196e007359841603ae8a7e4c9504bb4f3a6fa4a4
Abu Ehab <MyGlassBox@Hotmail.Com>
2014-11-27 00:24:43 +0200

    Debtors Class

---
+++ Solution1/XamaDataLayer/Accountant/DebtorsCmd.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamaDataLayer.Accountant
+{
+   public  class DebtorsCmd
+    {
+
+       static DbDataContext db = new DbDataContext();
+
+       public static bool AddDebt(Debtor tb)
+       {
+           db = new DbDataContext();
+           db.Debtors.InsertOnSubmit(tb);
+           db.SubmitChanges();
+           return true;
+       }
+
+       public static Debtor EditDebtor(Debtor tb, int xid)
+       {
+           db = new DbDataContext();
+           var q = db.Debtors.Where(d => d.ID == xid).SingleOrDefault();
+           q.DebtorName = tb.DebtorName;
+           q.Addresss = tb.Addresss;
+           q.PhoneNumber = tb.PhoneNumber;
+           q.AccountID = tb.AccountID;
+           q.DebtorDescription = tb.DebtorDescription;
+
+           db.SubmitChanges();
+
+           return q;
+       }
+
+       public static void DeleteDebtor(int xid)
+       {
+           db = new DbDataContext();
+           var q = db.Debtors.Where(d => d.ID == xid).SingleOrDefault();
+           db.Debtors.DeleteOnSubmit(q);
+
+           db.SubmitChanges();
+       }
+
+
+       public static List<Debtor> GetAllDebtors()
+       {
+           db = new DbDataContext();
+           return db.Debtors.ToList();
+       }
+    }
+}
