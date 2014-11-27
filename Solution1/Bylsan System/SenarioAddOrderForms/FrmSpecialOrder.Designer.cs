a86e0292e9b6a23b0968e23b65f0198eaf8eb15b
Abu Ehab <MyGlassBox@Hotmail.Com>
2014-11-27 23:05:58 +0200

    FrmSpecialOrder Part 1

--- Solution1/Bylsan System/SenarioAddOrderForms/FrmSpecialOrder.Designer.cs
+++ Solution1/Bylsan System/SenarioAddOrderForms/FrmSpecialOrder.Designer.cs
@@ -29,20 +29,20 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.DescriptiontextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.BrowseBtn = new Telerik.WinControls.UI.RadButton();
-            this.PathTextBox = new System.Windows.Forms.TextBox();
-            this.radButton1 = new Telerik.WinControls.UI.RadButton();
-            this.label2 = new System.Windows.Forms.Label();
-            this.OkyBtn = new Telerik.WinControls.UI.RadButton();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
+            this.OkyBtn = new Telerik.WinControls.UI.RadButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BrwoseBtn = new Telerik.WinControls.UI.RadButton();
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.BrowseBtn = new Telerik.WinControls.UI.RadButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DescriptiontextBox = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.BrowseBtn)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.OkyBtn)).BeginInit();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.OkyBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrwoseBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrowseBtn)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -50,7 +50,7 @@
             this.groupBox1.Controls.Add(this.panel1);
             this.groupBox1.Controls.Add(this.OkyBtn);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.radButton1);
+            this.groupBox1.Controls.Add(this.BrwoseBtn);
             this.groupBox1.Controls.Add(this.PathTextBox);
             this.groupBox1.Controls.Add(this.BrowseBtn);
             this.groupBox1.Controls.Add(this.label1);
@@ -60,63 +60,6 @@
             this.groupBox1.Size = new System.Drawing.Size(461, 403);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            // 
-            // DescriptiontextBox
-            // 
-            this.DescriptiontextBox.Location = new System.Drawing.Point(76, 19);
-            this.DescriptiontextBox.Multiline = true;
-            this.DescriptiontextBox.Name = "DescriptiontextBox";
-            this.DescriptiontextBox.Size = new System.Drawing.Size(368, 115);
-            this.DescriptiontextBox.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Description:";
-            // 
-            // BrowseBtn
-            // 
-            this.BrowseBtn.Location = new System.Drawing.Point(375, 163);
-            this.BrowseBtn.Name = "BrowseBtn";
-            this.BrowseBtn.Size = new System.Drawing.Size(59, 24);
-            this.BrowseBtn.TabIndex = 2;
-            this.BrowseBtn.Text = "...";
-            // 
-            // PathTextBox
-            // 
-            this.PathTextBox.Location = new System.Drawing.Point(82, 163);
-            this.PathTextBox.Name = "PathTextBox";
-            this.PathTextBox.Size = new System.Drawing.Size(287, 20);
-            this.PathTextBox.TabIndex = 3;
-            // 
-            // radButton1
-            // 
-            this.radButton1.Location = new System.Drawing.Point(324, 205);
-            this.radButton1.Name = "radButton1";
-            this.radButton1.Size = new System.Drawing.Size(110, 24);
-            this.radButton1.TabIndex = 4;
-            this.radButton1.Text = "Upload Image";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 166);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Select image:";
-            // 
-            // OkyBtn
-            // 
-            this.OkyBtn.Location = new System.Drawing.Point(345, 373);
-            this.OkyBtn.Name = "OkyBtn";
-            this.OkyBtn.Size = new System.Drawing.Size(110, 24);
-            this.OkyBtn.TabIndex = 6;
-            this.OkyBtn.Text = "Oky";
             // 
             // panel1
             // 
@@ -135,6 +78,65 @@
             this.label3.TabIndex = 0;
             this.label3.Text = "هان شوف الاداة المناسبة اللي  تتبين الصور اللي ارتفعت";
             // 
+            // OkyBtn
+            // 
+            this.OkyBtn.Location = new System.Drawing.Point(345, 373);
+            this.OkyBtn.Name = "OkyBtn";
+            this.OkyBtn.Size = new System.Drawing.Size(110, 24);
+            this.OkyBtn.TabIndex = 6;
+            this.OkyBtn.Text = "Oky";
+            this.OkyBtn.Click += new System.EventHandler(this.OkyBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Select image:";
+            // 
+            // BrwoseBtn
+            // 
+            this.BrwoseBtn.Location = new System.Drawing.Point(324, 205);
+            this.BrwoseBtn.Name = "BrwoseBtn";
+            this.BrwoseBtn.Size = new System.Drawing.Size(110, 24);
+            this.BrwoseBtn.TabIndex = 4;
+            this.BrwoseBtn.Text = "Upload Image";
+            this.BrwoseBtn.Click += new System.EventHandler(this.BrwoseBtn_Click);
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.Location = new System.Drawing.Point(82, 163);
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.Size = new System.Drawing.Size(287, 20);
+            this.PathTextBox.TabIndex = 3;
+            // 
+            // BrowseBtn
+            // 
+            this.BrowseBtn.Location = new System.Drawing.Point(375, 163);
+            this.BrowseBtn.Name = "BrowseBtn";
+            this.BrowseBtn.Size = new System.Drawing.Size(59, 24);
+            this.BrowseBtn.TabIndex = 2;
+            this.BrowseBtn.Text = "...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Description:";
+            // 
+            // DescriptiontextBox
+            // 
+            this.DescriptiontextBox.Location = new System.Drawing.Point(76, 19);
+            this.DescriptiontextBox.Multiline = true;
+            this.DescriptiontextBox.Name = "DescriptiontextBox";
+            this.DescriptiontextBox.Size = new System.Drawing.Size(368, 115);
+            this.DescriptiontextBox.TabIndex = 0;
+            // 
             // FrmSpecialOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -142,14 +144,15 @@
             this.ClientSize = new System.Drawing.Size(484, 427);
             this.Controls.Add(this.groupBox1);
             this.Name = "FrmSpecialOrder";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmSpecialOrder";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.BrowseBtn)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.OkyBtn)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.OkyBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrwoseBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrowseBtn)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -165,6 +168,6 @@
         private System.Windows.Forms.Label label3;
         private Telerik.WinControls.UI.RadButton OkyBtn;
         private System.Windows.Forms.Label label2;
-        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadButton BrwoseBtn;
     }
 }
--- Solution1/Bylsan System/SenarioAddOrderForms/FrmSpecialOrder.cs
+++ Solution1/Bylsan System/SenarioAddOrderForms/FrmSpecialOrder.cs
@@ -8,6 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO;
+using XamaDataLayer;
+using XamaDataLayer.BranchCmd;
+using XamaDataLayer.Helper_Classes;
+
+
 namespace Bylsan_System.SenarioAddOrderForms
 {
     public partial class FrmSpecialOrder : Form
@@ -16,5 +22,58 @@ namespace Bylsan_System.SenarioAddOrderForms
         {
             InitializeComponent();
         }
+
+   
+
+        #region "    ^^^ Brwose Photo    "
+
+        OpenFileDialog Op = new OpenFileDialog();
+        private void BrwoseBtn_Click(object sender, EventArgs e)
+        {
+            Op = new OpenFileDialog();
+            if (Op.ShowDialog() == DialogResult.OK)
+            {
+                this.Cursor = Cursors.WaitCursor;
+                Op.Filter = "Image Files(*.png; *.jpg; *.bmp)|*.png; *.jpg; *.bmp";
+                productpictureBox.Image = Image.FromFile(Op.FileName);
+                this.Cursor = Cursors.Default;
+
+            }
+        }
+     
+        byte[] byteImg1;
+        private void ConvertCarsPhotoes()
+        {
+
+            if (Op.FileName != "")
+            {
+
+                this.Cursor = Cursors.WaitCursor;
+                Image img = Image.FromFile(Op.FileName);
+                byteImg1 = PhotosConverter.ImageToByteArray(img);
+                this.Cursor = Cursors.Default;
+
+            }
+            else
+            {
+                byteImg1 = null;
+            }
+
+        }
+
+
+        #endregion 
+
+        private void OkyBtn_Click(object sender, EventArgs e)
+        {
+            // Starting Save :
+
+
+
+            //=====================================
+            this.Hide();
+        }
+
+
     }
 }
