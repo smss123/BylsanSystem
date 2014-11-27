8394e006b6bb7eada16febc169b873f31489265f
Abu Ehab <MyGlassBox@Hotmail.Com>
2014-11-27 21:09:40 +0200

    FrmPrintQuotation

--- Solution1/Bylsan System/SenarioAddOrderForms/FrmPrintQuotation.Designer.cs
+++ Solution1/Bylsan System/SenarioAddOrderForms/FrmPrintQuotation.Designer.cs
@@ -28,20 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.Label quotationForLabel;
             System.Windows.Forms.Label phoneNumberLabel;
             System.Windows.Forms.Label addressForLabel;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrintQuotation));
             this.quotationForTextBox = new System.Windows.Forms.TextBox();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
             this.addressForTextBox = new System.Windows.Forms.TextBox();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ListViewProducts = new System.Windows.Forms.ListView();
             this.TreeCategories = new System.Windows.Forms.TreeView();
-            this.ListViewProductes = new System.Windows.Forms.ListView();
             this.panel1 = new System.Windows.Forms.Panel();
             this.PrintBtn = new Telerik.WinControls.UI.RadButton();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             quotationForLabel = new System.Windows.Forms.Label();
             phoneNumberLabel = new System.Windows.Forms.Label();
             addressForLabel = new System.Windows.Forms.Label();
@@ -60,6 +63,24 @@
             quotationForLabel.TabIndex = 1;
             quotationForLabel.Text = "For:";
             // 
+            // phoneNumberLabel
+            // 
+            phoneNumberLabel.AutoSize = true;
+            phoneNumberLabel.Location = new System.Drawing.Point(12, 53);
+            phoneNumberLabel.Name = "phoneNumberLabel";
+            phoneNumberLabel.Size = new System.Drawing.Size(81, 13);
+            phoneNumberLabel.TabIndex = 2;
+            phoneNumberLabel.Text = "Phone Number:";
+            // 
+            // addressForLabel
+            // 
+            addressForLabel.AutoSize = true;
+            addressForLabel.Location = new System.Drawing.Point(24, 79);
+            addressForLabel.Name = "addressForLabel";
+            addressForLabel.Size = new System.Drawing.Size(69, 13);
+            addressForLabel.TabIndex = 4;
+            addressForLabel.Text = "Address For:";
+            // 
             // quotationForTextBox
             // 
             this.quotationForTextBox.Location = new System.Drawing.Point(99, 24);
@@ -71,7 +92,7 @@
             // 
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(644, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(769, 25);
             this.toolStrip1.TabIndex = 3;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -79,7 +100,7 @@
             // 
             this.statusStrip1.Location = new System.Drawing.Point(0, 590);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(644, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(769, 22);
             this.statusStrip1.TabIndex = 4;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -94,35 +115,10 @@
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox1.Location = new System.Drawing.Point(0, 25);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(644, 120);
+            this.groupBox1.Size = new System.Drawing.Size(769, 120);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Quotation Information ";
-            // 
-            // phoneNumberLabel
-            // 
-            phoneNumberLabel.AutoSize = true;
-            phoneNumberLabel.Location = new System.Drawing.Point(12, 53);
-            phoneNumberLabel.Name = "phoneNumberLabel";
-            phoneNumberLabel.Size = new System.Drawing.Size(81, 13);
-            phoneNumberLabel.TabIndex = 2;
-            phoneNumberLabel.Text = "Phone Number:";
-            // 
-            // phoneNumberTextBox
-            // 
-            this.phoneNumberTextBox.Location = new System.Drawing.Point(99, 50);
-            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
-            this.phoneNumberTextBox.Size = new System.Drawing.Size(181, 20);
-            this.phoneNumberTextBox.TabIndex = 3;
-            // 
-            // addressForLabel
-            // 
-            addressForLabel.AutoSize = true;
-            addressForLabel.Location = new System.Drawing.Point(24, 79);
-            addressForLabel.Name = "addressForLabel";
-            addressForLabel.Size = new System.Drawing.Size(69, 13);
-            addressForLabel.TabIndex = 4;
-            addressForLabel.Text = "Address For:";
             // 
             // addressForTextBox
             // 
@@ -131,20 +127,43 @@
             this.addressForTextBox.Size = new System.Drawing.Size(300, 20);
             this.addressForTextBox.TabIndex = 5;
             // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(99, 50);
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(181, 20);
+            this.phoneNumberTextBox.TabIndex = 3;
+            // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.ListViewProductes);
+            this.groupBox2.Controls.Add(this.ListViewProducts);
             this.groupBox2.Controls.Add(this.TreeCategories);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox2.Location = new System.Drawing.Point(0, 145);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(644, 356);
+            this.groupBox2.Size = new System.Drawing.Size(769, 356);
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Product";
             // 
+            // ListViewProducts
+            // 
+            this.ListViewProducts.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ListViewProducts.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ListViewProducts.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ListViewProducts.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListViewProducts.ForeColor = System.Drawing.Color.Blue;
+            this.ListViewProducts.FullRowSelect = true;
+            this.ListViewProducts.GridLines = true;
+            this.ListViewProducts.Location = new System.Drawing.Point(175, 16);
+            this.ListViewProducts.Name = "ListViewProducts";
+            this.ListViewProducts.Size = new System.Drawing.Size(591, 337);
+            this.ListViewProducts.TabIndex = 8;
+            this.ListViewProducts.UseCompatibleStateImageBehavior = false;
+            // 
             // TreeCategories
             // 
+            this.TreeCategories.Cursor = System.Windows.Forms.Cursors.Hand;
             this.TreeCategories.Dock = System.Windows.Forms.DockStyle.Left;
             this.TreeCategories.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TreeCategories.LineColor = System.Drawing.Color.Blue;
@@ -152,21 +171,8 @@
             this.TreeCategories.Name = "TreeCategories";
             this.TreeCategories.Size = new System.Drawing.Size(166, 337);
             this.TreeCategories.TabIndex = 7;
-            // 
-            // ListViewProductes
-            // 
-            this.ListViewProductes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.ListViewProductes.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.ListViewProductes.Dock = System.Windows.Forms.DockStyle.Right;
-            this.ListViewProductes.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ListViewProductes.ForeColor = System.Drawing.Color.Blue;
-            this.ListViewProductes.FullRowSelect = true;
-            this.ListViewProductes.GridLines = true;
-            this.ListViewProductes.Location = new System.Drawing.Point(184, 16);
-            this.ListViewProductes.Name = "ListViewProductes";
-            this.ListViewProductes.Size = new System.Drawing.Size(457, 337);
-            this.ListViewProductes.TabIndex = 8;
-            this.ListViewProductes.UseCompatibleStateImageBehavior = false;
+            this.TreeCategories.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.TreeCategories_NodeMouseClick);
+            this.TreeCategories.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.TreeCategories_NodeMouseDoubleClick);
             // 
             // panel1
             // 
@@ -174,23 +180,32 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 501);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(644, 89);
+            this.panel1.Size = new System.Drawing.Size(769, 89);
             this.panel1.TabIndex = 7;
             // 
             // PrintBtn
             // 
-            this.PrintBtn.Location = new System.Drawing.Point(488, 27);
+            this.PrintBtn.Location = new System.Drawing.Point(613, 28);
             this.PrintBtn.Name = "PrintBtn";
             this.PrintBtn.Size = new System.Drawing.Size(144, 46);
             this.PrintBtn.TabIndex = 1;
             this.PrintBtn.Text = "Print";
             this.PrintBtn.ThemeName = "Windows8";
+            this.PrintBtn.Click += new System.EventHandler(this.PrintBtn_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "eye.png");
+            this.imageList1.Images.SetKeyName(1, "award_star_bronze_2.png");
+            this.imageList1.Images.SetKeyName(2, "yen coins.ico");
             // 
             // FrmPrintQuotation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(644, 612);
+            this.ClientSize = new System.Drawing.Size(769, 612);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -198,6 +213,7 @@
             this.Controls.Add(this.toolStrip1);
             this.Name = "FrmPrintQuotation";
             this.Text = "FrmPrintQuotation";
+            this.Load += new System.EventHandler(this.FrmPrintQuotation_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
@@ -218,8 +234,9 @@
         private System.Windows.Forms.TextBox phoneNumberTextBox;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.TreeView TreeCategories;
-        private System.Windows.Forms.ListView ListViewProductes;
+        private System.Windows.Forms.ListView ListViewProducts;
         private System.Windows.Forms.Panel panel1;
         private Telerik.WinControls.UI.RadButton PrintBtn;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
