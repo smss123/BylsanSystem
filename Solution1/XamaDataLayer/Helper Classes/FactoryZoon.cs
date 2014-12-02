d60b7c67dcd854877a52a3f25703bff60c5c3663
Abu Ehab <MyGlassBox@Hotmail.Com>
2014-12-02 16:54:33 +0200

    FactoryZoon Class

---
+++ Solution1/XamaDataLayer/Helper Classes/FactoryZoon.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamaDataLayer;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace XamaDataLayer.Helper_Classes
+{
+    public class FactoryZoon 
+    {
+        static DbDataContext db = new DbDataContext();
+
+        public static List<OrderProduct> GetAllPrinting()
+        {
+
+            var lst = (from c in db.OrderProducts
+                       where c.Status == "Printing"
+                       select c).ToList();
+            return lst;
+
+        }
+    public static List<OrderProduct> GetAllInDesigner(){
+
+              var lst = ( from c in db.OrderProducts 
+                             where c.Status == "In Designer" 
+                             select c).ToList ();            
+              return lst ; 
+
+        }
+
+
+
+
+    public static List<OrderProduct> GetAllInDesigner() {
+
+        var lst = (from c in db.OrderProducts
+                   where c.Status == "In Producting"
+                   select c).ToList();
+        return lst; 
+    }
+
+
+    public static List<OrderProduct> GetAllToDeliver()
+    {
+
+        var lst = (from c in db.OrderProducts
+                   where c.Status == "To Deliver"
+                   select c).ToList();
+        return lst;
+    }                   
+        
+      
+        
+    }
+}
