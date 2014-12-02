110ed280a59ba14053fc036ad08b6638c90cc305
smss123 <smsssamer@live.com>
2014-12-02 16:00:36 +0400

    Add Excepion Event

--- Solution1/Bylsan System/Program.cs
+++ Solution1/Bylsan System/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Bylsan_System.SenarioAddOrderForms;
+using Telerik.WinControls;
 namespace Bylsan_System
 {
     static class Program
@@ -18,7 +19,13 @@ namespace Bylsan_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += Application_ThreadException;
             Application.Run(new FrmLogin());
+        }
+
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            RadMessageBox.Show("Oops , Same error here,\n"+e.Exception.ToString(), "error");
         }
     }
 }
