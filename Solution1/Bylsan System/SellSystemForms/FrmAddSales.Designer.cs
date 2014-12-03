57c40f483d86f17e5097b530dd614ca8da81ef21
ainma007 <b-sword@outlook.com>
2014-12-03 15:58:14 +0200

    AddSalesForm

---
+++ Solution1/Bylsan System/SellSystemForms/FrmAddSales.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Bylsan_System.SellSystemForms
+{
+    partial class FrmAddSales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ProductlistView = new System.Windows.Forms.ListView();
+            this.OrderGridView = new Telerik.WinControls.UI.RadGridView();
+            this.Okybtn = new Telerik.WinControls.UI.RadButton();
+            this.visualStudio2012LightTheme1 = new Telerik.WinControls.Themes.VisualStudio2012LightTheme();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.OrderGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrderGridView.MasterTemplate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Okybtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(827, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 600);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(827, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ProductlistView);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(827, 296);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "product Show";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Okybtn);
+            this.groupBox2.Controls.Add(this.OrderGridView);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 321);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(827, 279);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Order";
+            // 
+            // ProductlistView
+            // 
+            this.ProductlistView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ProductlistView.Location = new System.Drawing.Point(3, 18);
+            this.ProductlistView.Name = "ProductlistView";
+            this.ProductlistView.Size = new System.Drawing.Size(821, 275);
+            this.ProductlistView.TabIndex = 0;
+            this.ProductlistView.UseCompatibleStateImageBehavior = false;
+            // 
+            // OrderGridView
+            // 
+            this.OrderGridView.Dock = System.Windows.Forms.DockStyle.Left;
+            this.OrderGridView.Location = new System.Drawing.Point(3, 18);
+            this.OrderGridView.Name = "OrderGridView";
+            this.OrderGridView.Size = new System.Drawing.Size(602, 258);
+            this.OrderGridView.TabIndex = 0;
+            this.OrderGridView.Text = "radGridView1";
+            this.OrderGridView.ThemeName = "VisualStudio2012Light";
+            // 
+            // Okybtn
+            // 
+            this.Okybtn.Location = new System.Drawing.Point(692, 235);
+            this.Okybtn.Name = "Okybtn";
+            this.Okybtn.Size = new System.Drawing.Size(129, 38);
+            this.Okybtn.TabIndex = 1;
+            this.Okybtn.Text = "Oky";
+            this.Okybtn.ThemeName = "VisualStudio2012Light";
+            // 
+            // FrmAddSales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(827, 622);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "FrmAddSales";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "FrmAddSales";
+            this.ThemeName = "VisualStudio2012Light";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.OrderGridView.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrderGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Okybtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView ProductlistView;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private Telerik.WinControls.UI.RadButton Okybtn;
+        private Telerik.WinControls.UI.RadGridView OrderGridView;
+        private Telerik.WinControls.Themes.VisualStudio2012LightTheme visualStudio2012LightTheme1;
+    }
+}
