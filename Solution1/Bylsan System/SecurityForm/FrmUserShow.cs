72db216d2f34359f7bcc1e8cf2921e5e02d236c5
ainma007 <b-sword@outlook.com>
2014-12-06 18:49:31 +0200

    UserPermessionForms

---
+++ Solution1/Bylsan System/SecurityForm/FrmUserShow.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using XamaDataLayer;
+using XamaDataLayer.Security;
+
+namespace Bylsan_System.SecurityForm
+{
+    public partial class FrmUserShow : Telerik.WinControls.UI.RadForm
+    {
+        public FrmUserShow()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmUserShow_Load(object sender, EventArgs e)
+        {
+            Thread th = new Thread(FillDataUser);
+            th.Start();
+        }
+
+        private void FillDataUser()
+        {
+            statusStrip1.Invoke((MethodInvoker)delegate
+            {
+
+                toolStrip1.Text = "Loading .. ";
+
+            });
+            Operation.BeginOperation(this);
+
+            Application.DoEvents();
+            var q = UserCmd.GetAllUsers();
+            Application.DoEvents();
+
+            Operation.EndOperation(this);
+            statusStrip1.Invoke((MethodInvoker)delegate
+            {
+                UserGridView.DataSource = q;
+                toolStrip1.Text = "Compelete Load .... ";
+
+            });
+        }
+
+        private void UserGridView_CommandCellClick(object sender, EventArgs e)
+        {
+            FrmEditUserPermession frm = new FrmEditUserPermession();
+            frm.TragetUser = (User)UserGridView.CurrentRow.DataBoundItem;
+            frm.ShowDialog();
+        }
+    }
+}
