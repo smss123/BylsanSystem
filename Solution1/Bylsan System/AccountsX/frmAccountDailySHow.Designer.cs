81db167d57b31869e6bd8c143a5bfb4d857573cb
ainma007 <b-sword@outlook.com>
2014-12-14 10:46:41 +0200

    AccountDailyForm

---
+++ Solution1/Bylsan System/AccountsX/frmAccountDailySHow.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Bylsan_System.AccountsX
+{
+    partial class frmAccountDailySHow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Telerik.WinControls.UI.GridViewDecimalColumn gridViewDecimalColumn1 = new Telerik.WinControls.UI.GridViewDecimalColumn();
+            Telerik.WinControls.UI.GridViewDecimalColumn gridViewDecimalColumn2 = new Telerik.WinControls.UI.GridViewDecimalColumn();
+            Telerik.WinControls.UI.GridViewDecimalColumn gridViewDecimalColumn3 = new Telerik.WinControls.UI.GridViewDecimalColumn();
+            Telerik.WinControls.UI.GridViewDecimalColumn gridViewDecimalColumn4 = new Telerik.WinControls.UI.GridViewDecimalColumn();
+            Telerik.WinControls.UI.GridViewDateTimeColumn gridViewDateTimeColumn1 = new Telerik.WinControls.UI.GridViewDateTimeColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn1 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn2 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.visualStudio2012LightTheme1 = new Telerik.WinControls.Themes.VisualStudio2012LightTheme();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.AccountDailyGridView = new Telerik.WinControls.UI.RadGridView();
+            this.accountDailyBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AccountDailyGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AccountDailyGridView.MasterTemplate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.accountDailyBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(803, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 511);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(803, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.AccountDailyGridView);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(803, 486);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Account Daily Data";
+            // 
+            // AccountDailyGridView
+            // 
+            this.AccountDailyGridView.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.AccountDailyGridView.Cursor = System.Windows.Forms.Cursors.Default;
+            this.AccountDailyGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AccountDailyGridView.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.AccountDailyGridView.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.AccountDailyGridView.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.AccountDailyGridView.Location = new System.Drawing.Point(3, 18);
+            // 
+            // AccountDailyGridView
+            // 
+            this.AccountDailyGridView.MasterTemplate.AutoGenerateColumns = false;
+            this.AccountDailyGridView.MasterTemplate.AutoSizeColumnsMode = Telerik.WinControls.UI.GridViewAutoSizeColumnsMode.Fill;
+            gridViewDecimalColumn1.DataType = typeof(int);
+            gridViewDecimalColumn1.EnableExpressionEditor = false;
+            gridViewDecimalColumn1.FieldName = "ID";
+            gridViewDecimalColumn1.HeaderText = "ID";
+            gridViewDecimalColumn1.IsAutoGenerated = true;
+            gridViewDecimalColumn1.Name = "ID";
+            gridViewDecimalColumn1.Width = 112;
+            gridViewDecimalColumn2.DataType = typeof(string);
+            gridViewDecimalColumn2.EnableExpressionEditor = false;
+            gridViewDecimalColumn2.FieldName = "Account.AccountName";
+            gridViewDecimalColumn2.HeaderText = "AccountName";
+            gridViewDecimalColumn2.IsAutoGenerated = true;
+            gridViewDecimalColumn2.Name = "AccountName";
+            gridViewDecimalColumn2.Width = 112;
+            gridViewDecimalColumn3.DataType = typeof(System.Nullable<double>);
+            gridViewDecimalColumn3.EnableExpressionEditor = false;
+            gridViewDecimalColumn3.FieldName = "TotalIn";
+            gridViewDecimalColumn3.HeaderText = "TotalIn";
+            gridViewDecimalColumn3.IsAutoGenerated = true;
+            gridViewDecimalColumn3.Name = "TotalIn";
+            gridViewDecimalColumn3.Width = 112;
+            gridViewDecimalColumn4.DataType = typeof(System.Nullable<double>);
+            gridViewDecimalColumn4.EnableExpressionEditor = false;
+            gridViewDecimalColumn4.FieldName = "TotalOut";
+            gridViewDecimalColumn4.HeaderText = "TotalOut";
+            gridViewDecimalColumn4.IsAutoGenerated = true;
+            gridViewDecimalColumn4.Name = "TotalOut";
+            gridViewDecimalColumn4.Width = 112;
+            gridViewDateTimeColumn1.DataType = typeof(System.Nullable<System.DateTime>);
+            gridViewDateTimeColumn1.EnableExpressionEditor = false;
+            gridViewDateTimeColumn1.FieldName = "DateOfProcess";
+            gridViewDateTimeColumn1.Format = System.Windows.Forms.DateTimePickerFormat.Long;
+            gridViewDateTimeColumn1.HeaderText = "DateOfProcess";
+            gridViewDateTimeColumn1.IsAutoGenerated = true;
+            gridViewDateTimeColumn1.Name = "DateOfProcess";
+            gridViewDateTimeColumn1.Width = 112;
+            gridViewTextBoxColumn1.EnableExpressionEditor = false;
+            gridViewTextBoxColumn1.FieldName = "Description";
+            gridViewTextBoxColumn1.HeaderText = "Description";
+            gridViewTextBoxColumn1.IsAutoGenerated = true;
+            gridViewTextBoxColumn1.Name = "Description";
+            gridViewTextBoxColumn1.Width = 112;
+            gridViewTextBoxColumn2.EnableExpressionEditor = false;
+            gridViewTextBoxColumn2.FieldName = "CommandArg";
+            gridViewTextBoxColumn2.HeaderText = "CommandArg";
+            gridViewTextBoxColumn2.IsAutoGenerated = true;
+            gridViewTextBoxColumn2.Name = "CommandArg";
+            gridViewTextBoxColumn2.Width = 112;
+            this.AccountDailyGridView.MasterTemplate.Columns.AddRange(new Telerik.WinControls.UI.GridViewDataColumn[] {
+            gridViewDecimalColumn1,
+            gridViewDecimalColumn2,
+            gridViewDecimalColumn3,
+            gridViewDecimalColumn4,
+            gridViewDateTimeColumn1,
+            gridViewTextBoxColumn1,
+            gridViewTextBoxColumn2});
+            this.AccountDailyGridView.MasterTemplate.DataSource = this.accountDailyBindingSource;
+            this.AccountDailyGridView.MasterTemplate.EnableFiltering = true;
+            this.AccountDailyGridView.Name = "AccountDailyGridView";
+            this.AccountDailyGridView.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.AccountDailyGridView.Size = new System.Drawing.Size(797, 465);
+            this.AccountDailyGridView.TabIndex = 0;
+            this.AccountDailyGridView.Text = "radGridView1";
+            this.AccountDailyGridView.ThemeName = "VisualStudio2012Light";
+            // 
+            // accountDailyBindingSource
+            // 
+            this.accountDailyBindingSource.DataSource = typeof(XamaDataLayer.AccountDaily);
+            // 
+            // frmAccountDailySHow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(803, 533);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "frmAccountDailySHow";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmAccountDailySHow";
+            this.ThemeName = "VisualStudio2012Light";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.AccountDailyGridView.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AccountDailyGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.accountDailyBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private Telerik.WinControls.Themes.VisualStudio2012LightTheme visualStudio2012LightTheme1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Telerik.WinControls.UI.RadGridView AccountDailyGridView;
+        private System.Windows.Forms.BindingSource accountDailyBindingSource;
+    }
+}
