c1acb9c249f9d897eaca97cef70d3aa4d27f61af
ainma007 <b-sword@outlook.com>
2014-12-15 19:53:07 +0200

    OrderReportCod

--- Solution1/Bylsan System/Reports/ReportsObject/OrderReportObj.cs
+++ Solution1/Bylsan System/Reports/ReportsObject/OrderReportObj.cs
@@ -7,10 +7,11 @@ using XamaDataLayer;
 
 namespace Bylsan_System.Reports.ReportsObject
 {
-     public class OrderReportObj
+    public class OrderReportObj
     {
-         //Order
-         public Order RepOrder { get; set; }
+        //Order
+        public string CustomerName { get; set; }
+        public string CustomerPhoneNumber { get; set; }
         public int OrderIdID { get; set; }
         public string OrderName { get; set; }
         public string OrderType { get; set; }
@@ -21,18 +22,20 @@ namespace Bylsan_System.Reports.ReportsObject
         public string OrderStatus { get; set; }
         public double TotalAmount { get; set; }
         public DateTime OrderDate { get; set; }
-        public string DeliverdToBranch { get; set; }
-        public string CustomerName { get; set; }
+        public int DeliverdToBranch { get; set; }
+
 
         ////////////////OrderProduct
 
 
         public string ProductName { get; set; }
-        public int  Qty { get; set; }
-       // public string ProductName { get; set; }
-       // public string ProductName { get; set; }
-//
+        public int Qty { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductUnit { get; set; }
+        public string PublicName { get; set; }
+        public string OrderProductDescription { get; set; }
+        public string OrderProductStatus { get; set; }
 
-         
+
     }
 }
