833662d27e5e8bf45728ba9b3251e5ddc4b5db76
ainma007 <b-sword@outlook.com>
2014-12-16 18:23:26 +0200

    ItemStorReport

---
+++ Solution1/Bylsan System/Reports/ReportsObject/ItemsStoreReportObj.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bylsan_System.Reports.ReportsObject
+{
+    public class ItemsStoreReportObj
+    {/////
+        public int StorId { get; set; }
+        public string itemName { get; set; }
+        public Int64 AvailableQty { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
