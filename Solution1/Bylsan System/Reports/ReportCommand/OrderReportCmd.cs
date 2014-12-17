275b78c1ad899bed1320d917648e114c217c1ddb
ainma007 <b-sword@outlook.com>
2014-12-17 16:51:04 +0200

    ExpenssesReport

--- Solution1/Bylsan System/Reports/ReportCommand/OrderReportCmd.cs
+++ Solution1/Bylsan System/Reports/ReportCommand/OrderReportCmd.cs
@@ -67,52 +67,52 @@ namespace Bylsan_System.Reports.ReportCommand
         }
 
         /////استخراج كافة الفواتير المفصلة الخاصة بزبون واحد   -	تقرير العميل وطلبياته
-        public void GetOrderProductsCustomer()
+        public void GetOrderProductsCustomerPhone(string Phone)
         {
-            ///GetAllOederBy Customer
-            ///وضع دالة الاستعلام الخاصة بالكستمر
-            //ReportDataSource rs = new ReportDataSource();
-            //List<OrderReportObj> ls = new List<OrderReportObj>();
 
-            //foreach (var item in q)
-            //{
+            var q = OrderProductsCmd.GetOrderProductByCustomerPhone(Phone);
+            ReportDataSource rs = new ReportDataSource();
+            List<OrderReportObj> ls = new List<OrderReportObj>();
 
-            //    ls.Add(new OrderReportObj()
-            //    {////////////
-            //         information Order And Customer
-            //        OrderIdID = item.Order.ID,
-            //        CustomerName = item.Order.Customer.CustomerName,
-            //        CustomerPhoneNumber = item.Order.Customer.PhoneNumber,
-            //        OrderName = item.Order.OrderName,
-            //        OrderType = item.Order.OrderType,
-            //        OrderDate = item.Order.OrderDate.Value,
-            //        OrderDelivery = item.Order.OrderDelivery,
-            //        OrderVerify = item.Order.OrderVerify,
-            //        OrderDeliveryDate = item.Order.OrderDeliveryDate.Value,
-            //        OrderStatus = item.Order.OrderStatus,
-            //        TotalAmount = item.Order.TotalAmount.Value,
-            //        DeliverdToBranch = item.Order.DeliverdToBranch.Value,
+            foreach (var item in q)
+            {
 
-            //        //informationOrderPrduct
-            //        ProductName = item.Product.Product_Name,
-            //        Qty = item.Qty.Value,
-            //        ProductPrice = item.Product.ProductPrice.Value,
-            //        PublicName = item.Product.PublicName,
-            //        OrderProductDescription = item.Description,
-            //        OrderProductStatus = item.Status,
+                ls.Add(new OrderReportObj()
+                {////////////
+                //     information Order And Customer
+                    OrderIdID = item.Order.ID,
+                    CustomerName = item.Order.Customer.CustomerName,
+                    CustomerPhoneNumber = item.Order.Customer.PhoneNumber,
+                    OrderName = item.Order.OrderName,
+                    OrderType = item.Order.OrderType,
+                    OrderDate = item.Order.OrderDate.Value,
+                    OrderDelivery = item.Order.OrderDelivery,
+                    OrderVerify = item.Order.OrderVerify,
+                    OrderDeliveryDate = item.Order.OrderDeliveryDate.Value,
+                    OrderStatus = item.Order.OrderStatus,
+                    TotalAmount = item.Order.TotalAmount.Value,
+                    DeliverdToBranch = item.Order.DeliverdToBranch.Value,
 
-
+                   // informationOrderPrduct
+                    ProductName = item.Product.Product_Name,
+                    Qty = item.Qty.Value,
+                    ProductPrice = item.Product.ProductPrice.Value,
+                    PublicName = item.Product.PublicName,
+                    OrderProductDescription = item.Description,
+                    OrderProductStatus = item.Status,
 
 
-            //    });
-            //}
-            //rs.Name = "DataSet1";
-            //rs.Value = ls;
-            //RebortView frm = new RebortView();
-            //frm.reportViewer1.LocalReport.DataSources.Clear();
-            //frm.reportViewer1.LocalReport.DataSources.Add(rs);
-            //frm.reportViewer1.LocalReport.ReportEmbeddedResource = "Bylsan_System.Reports.Sheets.RepOrderFollow.rdlc";
-            //frm.ShowDialog();
+
+
+                });
+            }
+            rs.Name = "DataSet1";
+            rs.Value = ls;
+            RebortView frm = new RebortView();
+            frm.reportViewer1.LocalReport.DataSources.Clear();
+            frm.reportViewer1.LocalReport.DataSources.Add(rs);
+            frm.reportViewer1.LocalReport.ReportEmbeddedResource = "Bylsan_System.Reports.Sheets.RepOrderFollow.rdlc";
+            frm.ShowDialog();
 
 
         }
