927fdb2aec2e68eacf3352bfad90d5d8fd275a22
ainma007 <b-sword@outlook.com>
2014-12-20 13:15:52 +0200

    سند الصرف

---
+++ Solution1/Bylsan System/Reports/ReportsObject/ExchangebondReportObj.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bylsan_System.Reports.ReportsObject
+{
+    public   class ExchangebondReportObj
+    {
+
+
+        public int bondNumber { get; set; }
+        public string bondName { get; set; }
+        public string bondDate { get; set; }
+        public double bondCost { get; set; }
+        public double Remaining { get; set; }
+    }
+}
